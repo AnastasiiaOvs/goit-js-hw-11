a9376b4e9a8c78d7c994b270536fe4af785c7608
AnastasiiaOvs <a.alexandrovna@i.ua>
2025-05-16 12:21:44 +0200

    initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '50324197-16ec26870062a9854cfe1529b';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
